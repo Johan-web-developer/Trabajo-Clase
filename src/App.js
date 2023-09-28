75d7e9249dff30a4f19598cb8b9b2074122c8dd8
Johan-web-developer <johan.flore.13@gmail.com>
2023-09-28 06:52:01 -0500

    style: :lipstick: estilos mejorados

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import './App.css'
 
-function App() {
+export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [images, setImages] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://dog.ceo/api/breeds/image/random/3");
+        if (response.ok) {
+          const data = await response.json();
+          setImages(data.message);
+          setIsLoading(false);
+          setError(null);
+        } else {
+          setError("No se pudieron obtener las imágenes");
+        }
+      } catch (error) {
+        setError("El API no está respondiendo");
+      }
+    };
+    fetchData();
+  }, [isLoading]);
+
+
+  const randomDog = () =>{
+    setIsLoading(true)
+  }
+
+  if (isLoading) {
+    return (
+      <div className='App'>
+        <h1>Cargando....</h1>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className='App'>
+        <h1>{error}</h1>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {images.map((url, index) => (
+        <img key={index} src={url} alt={`Dog ${index + 1}`} />
+      ))}
+      <button onClick={randomDog}>Otros Perritos</button>
     </div>
   );
 }
-
-export default App;
